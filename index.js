e7fe1056cd25e1c07a7d264c3658a280b487012e
Emilis Dambauskas <emilis.d@gmail.com>
2015-04-18 22:47:52 +0300

    Refactored. Split game into game engine and UI parts.

--- index.js
+++ index.js
@@ -1,95 +1,36 @@
-#!/usr/bin/env node
-
 /// Requirements ---------------------------------------------------------------
 
-var _ =                 require( "lodash" );
-var blessed =           require( 'blessed' );
+var screen =            require( "./lib/ui/screen" );
+var world =             require( "./lib/game/world" );
 
-var CONST =             require( "./lib/constants" );
-
-var debug =             require( "./lib/debugger" );
-
-var Player =            require( "./lib/world/player" );
-var Vector =            require( "./lib/geometry/vector" );
-var World =             require( "./lib/world/world" );
+var Player =            require( "./lib/game/player" );
 
 /// Constants ------------------------------------------------------------------
 
-var FPS =               60;
-var INTERVAL =          1000 / FPS;
-var PADDING =           10;
+var START_POINT = {
+    x:                  Math.pow( 2, 23 ),
+    y:                  Math.pow( 2, 23 ),
+};
 
 /// Main -----------------------------------------------------------------------
 
-// Create a screen object.
-var screen =            blessed.screen({
-    term:               "xterm-256color",
-    fullUnicode:        true,
-});
+var player1 =           new Player( "Player1", "PL", "blue", "yellow" );
+player1.setPos( START_POINT.x, START_POINT.y );
 
-debug.addToScreen( screen, {
-    top:                0,
-    right:              0,
-    width:              "40%",
-    height:             "100%",
-    border:             { type: "line" },
-    label:              "[ Debug ]",
-    content:            "",
-});
+world.addPlayer( player1 );
 
+screen.map.follow( player1, world );
+/// screen.map.render( world );
 
-// Create a box perfectly centered horizontally and vertically.
-var box =   blessed.box({
-    top:                0, ///'center',
-    left:               0, ///'center',
-    width:              "60%", ///CONST.width * CONST.pxwidth,
-    height:             "100%", /// CONST.height,
-    border:             { type: "line" },
-    label:              "[ World ]",
-    scrollable:         true,
-    alwaysScroll:       true,
-    tags:               true,
-    style: {
-        fg:             CONST.fg,
-        bg:             CONST.bg,
-    },
-    content:            "",
-});
+screen.screen.key([ "q", "C-c" ], quit );
 
-screen.append( box );
+screen.screen.key([ "left" ],  movePlayer( player1, "left" ));
+screen.screen.key([ "right" ], movePlayer( player1, "right" ));
+screen.screen.key([ "up" ],    movePlayer( player1, "up" ));
+screen.screen.key([ "down" ],  movePlayer( player1, "down" ));
 
-var thFrame =           _.throttle( frame, INTERVAL );
-
-var viewport =          new Vector( 0, 0, 0, 0 );
-resizeViewport();
-screen.on( "resize", resizeViewport )
-
-
-screen.key([ 'q', 'C-c' ], quit );
-
-/// Add player:
-
-var player1 =            new Player( "Player1", "#66f", "yellow" );
-player1.setPos( 10, 20 );
-World.addPlayer( player1 );
-
-screen.key([ "left" ],  movePlayer( player1, "left" ));
-screen.key([ "right" ], movePlayer( player1, "right" ));
-screen.key([ "up" ],    movePlayer( player1, "up" ));
-screen.key([ "down" ],  movePlayer( player1, "down" ));
-screen.key([ "space" ], function(){ player1.draw();     thFrame(); });
-
-/// Redraw:
-
-thFrame();
 
 /// Functions ------------------------------------------------------------------
-
-function frame(){
-
-    box.setContent( World.getState( viewport ));
-    screen.render();
-}///
 
 function quit(){
 
@@ -101,23 +42,7 @@ function movePlayer( player, direction ){
     return function(){
 
         player[direction]();
-
-        viewport.x0 =   Math.min( viewport.x0, player.x - PADDING );
-        viewport.y0 =   Math.min( viewport.y0, player.y - PADDING );
-        if( player.x + PADDING > viewport.x1 ){
-            viewport.x0 += viewport.x1 - player.x;
-        }
-        if( player.y + PADDING > viewport.y1 ){
-            viewport.y0 += viewport.y1 - player.y;
-        }
-        resizeViewport();
+        /// screen.map.follow( player1 );
+        /// screen.map.render( world );
     };//
-}///
-
-
-function resizeViewport(){
-
-    viewport.x1 =       viewport.x0 + Math.floor( box.width / 2 ) - 1;
-    viewport.y1 =       viewport.y0 + box.height - 2;
-    thFrame();
 }///
